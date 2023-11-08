c2827a1c40849c0544b54facf4bbe56dc4ad477a
vleadadmin <admin@vlabs.ac.in>
2023-11-08 04:54:27 +0000

    https://virtual-labs.github.io/virtual-labs/exp-orifice-plates-iitkgp click on the link to test your code.

---
+++ simulation/js/addinptotable.js
@@ -0,0 +1,114 @@
+/* Lab:Mine Automation and Virtual Reatlity
+ Exp: Calibration of orifice plates
+	 File name: addinptotable.js
+   Author:Prakriti Dhang
+*/
+
+var  otable;
+var arr=[];
+var tabrowindex=0;
+var nofob ;
+var rows,x;
+var mvelr, aqr, dm, aration, pdiff,equ1,airflow,coeff;
+
+function addresult(){
+    document.getElementById('obvtable').style.display="block";
+  mvelr= document.getElementById('mvelr').value;
+ aqr =document.getElementById('aqr').value;
+  dm= document.getElementById('dm').value;
+ aration= document.getElementById('aration').value;
+ pdiff= document.getElementById('pdiff').value;
+ airflow= document.getElementById('airflow').value;
+coeff=  document.getElementById('coeff').value;
+      
+ otable= document.getElementById('obtable')
+ arr[0]=obsnt;
+ arr[1] =mvelr;
+ arr[2] =pdiff;
+ arr[3]=equ1;
+ arr[4]=airflow;
+ arr[5]=coeff;
+
+ tabrowindex + 1;
+
+var row = otable.insertRow(++tabrowindex); // Row increment
+    for (var q = 0; q < 6; q++) {
+
+        var cell = row.insertCell(q);
+        cell.innerHTML = arr[q];
+        //
+    }
+ 
+  document.getElementById('anotherread').style.display="block";
+  
+   document.getElementById('measurebtn').disabled="true";
+    
+    document.getElementById('btnobv').disabled ="true";
+ 
+
+    /*  Scroll down and after 3000 ms scroll up  */
+    document.body.scrollBy(0,550);
+    document.documentElement.scrollBy(0,500);
+    setTimeout(function(){
+        document.body.scrollTop = 50;
+        document.documentElement.scrollTop =50;
+    }, 3000);
+
+   
+    rows = otable.rows;
+  for (i = 1; i < (rows.length ); i++) {
+   
+    x = rows[i].getElementsByTagName("td")[0];
+   
+    if (Number(x.innerHTML) == 5) {
+      document.getElementById('anotherread').style.display="block";
+      document.getElementById('cardr').style.display="none";
+   document.getElementById('pltbtn').style.display="block";
+   document.getElementById('yesbtn').style.display="none";
+       
+      document.getElementById('graphd').style.display="block";
+   document.getElementById('anotherread').style.display="none";
+   document.getElementById('pltbtn').style.display="none";
+   document.getElementById('yesbtn').style.display="none";
+   
+  
+   document.getElementById('velr1').value='';
+  document.getElementById('velr2').value='';
+  document.getElementById('velr3').value='';
+
+  document.getElementById('mvelr').value='';
+  document.getElementById('aqr').value='';
+  document.getElementById('dm').value='';
+  document.getElementById('aration').value='';
+  document.getElementById('pdiff').value='';
+  document.getElementById('airflow').value='';
+  document.getElementById('coeff').value='';
+  document.getElementById('btnobv').disabled=true;
+  document.getElementById('measure').style.display="none";
+  document.getElementById('datam').style.display="none";
+  document.getElementById('obvtable').style.display="block";
+  document.getElementById('controlpanelv').style.display="none";
+  document.getElementById('anmbtn').disabled=true;
+  document.getElementById('obsanem').style.display="none";
+   //document.getElementById('graphd').style.display="block";
+       //document.getElementById('formulashow').style.display="block";
+    }
+    else if(Number(x.innerHTML) < 5) {
+        document.getElementById('graphd').style.display="none";
+        
+      }
+  }
+ 
+
+    }
+
+    
+
+  
+
+
+    
+
+       
+       
+     
---
+++ theory.md
@@ -0,0 +1,32 @@
+## Theory
+
+Plate of orifice, containing a concentric circular cut, place into duct such that the plate of orifice is concentric with the duct (Figure1). Air quantity following through the duct with the presence of plate of orifice is formulated as
+
+$$ Q= \alpha A_2 \sqrt{\frac{2(\delta P)}{\rho - (1-m^2)}}  -------------------- (1) $$
+
+Where,
+α = coefficient of discharge  
+A2 = Area of cross-section of the opening of the orifice plate  
+ΔP = Static pressure difference of the two sides of orifice plate i.e. (P1−P2). In this case, P1 is the static pressure   before the plate and P2 is the static pressure after the plate.  
+ρ = Density of medium; in this case the medium is air so its value is 1.  
+m = Cross-section area ratio of plate of orifice opening with the duct opening = A2/A1 where as A1 is the cross-section   area of the opening of the duct. In this experiment the design is in such a way that the value of m is 0.5.  
+In this experiment, the discharge coefficient will be determined from the equation1 with varying the air quantity by changing the throttling device at the end of the duct and calculating pressure difference by measuring pressures at the two sides of the plate of orifice. Air quantity will be determined by multiplying the area with air velocity measured by the anemometer. So from the equation1 the discharge coefficient α will be determined which is given below in equation2.  
+
+$$ \alpha = \frac{Q}{A_2 \sqrt{\frac{2(\delta P)}{\rho - (1-m^2)}}} ----------------- (2) $$
+
+#### **Instruments:**
+The required instruments in this experiment are given below.
+
+1. Orifice plate (Situated into the duct)
+2. Anemometer
+3. Throttling devices
+4. U tube manometer
+5. Measuring tape
+
+## Demo
+
+<figure class="video_container">
+  <iframe src="https://www.youtube.com/embed/uSUNMVih_kE" frameborder="0" allowfullscreen="true"> </iframe>
+</figure>
+
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
